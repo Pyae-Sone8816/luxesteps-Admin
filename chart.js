72ae4c033cce1bfb434dd605372503edcc91204a
Pyae-Sone8816 <pyaesoneaung8816@gmail.com>
2025-02-26 19:56:58 +0630

    first commit

---
+++ chart.js
@@ -0,0 +1,18 @@
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: "World Wide Wine Production",
+    },
+  },
+});
